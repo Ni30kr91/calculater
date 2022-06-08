4835516bec288e19f196ac42caaf52d92c955b57
Ni30kr91 <nitish.kumar8092@gmail.com>
2022-06-08 17:45:11 +0530

    calculated data

--- cal.go
+++ cal.go
@@ -5,10 +5,18 @@ package main
 import "fmt"
 
 func main() {
-	num1 := 30
-	num2 := 50
-	result := 0
-	operation := "+"
+	var num1 int 
+	var num2 int 
+	var operation string
+	fmt.Println("Enter First Number: ")
+	fmt.Scanln(&num1)
+	fmt.Println("Enter Second Number: ")
+	fmt.Scanln(&num2)
+	fmt.Println("Enter operation")
+	fmt.Scanln(&operation)
+
+	var result int 
+
 	if operation == "+" {
 		result = num1 + num2
 	}
